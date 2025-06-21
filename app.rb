53dc82c1fc537d57aac2e899af970494ecd4d778
A <bloodaxed@gmail.com>
2025-06-21 18:27:19 +0000

    built the whole thing

--- app.rb
+++ app.rb
@@ -1,9 +1,64 @@
 require "sinatra"
 require "sinatra/reloader"
 
+RPS = ["Rock", "Paper", "Scissors"]
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules, { :layout => :layout })
+end
+
+get("/rock") do
+  
+  @p_play = "Rock"
+  @cpu_play = RPS[rand(3)]
+  @outcome = score("r", @cpu_play[0].downcase)
+
+  erb(:play, { :layout => :layout })
+end
+
+get("/paper") do
+
+  @p_play = "Paper"
+  @cpu_play = RPS[rand(3)]
+  @outcome = score("p", @cpu_play[0].downcase)
+
+  erb(:play, { :layout => :layout })
+end
+
+get("/scissors") do
+
+  @p_play = "Scissors"
+  @cpu_play = RPS[rand(3)]
+  @outcome = score("s", @cpu_play[0].downcase)
+
+  erb(:play, { :layout => :layout })
+end
+
+def score(p_throw,c_throw)
+    #0 == player win
+    #1 == player loss
+    #2 == draw
+    if p_throw == c_throw
+        return 2
+    elsif p_throw == "r"
+        if c_throw == "s"
+            return 0
+        else
+            return 1
+        end
+    elsif p_throw == "p"
+        if c_throw == "r"
+            return 0
+        else
+            return 1
+        end
+    elsif p_throw == "s"
+        if c_throw == "p"
+            return 0
+        else
+            return 1
+        end
+    else #in case we get here without p_throw being valid
+        puts "bad input"
+    end
 end
